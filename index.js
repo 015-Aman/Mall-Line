b3669f4993e18f41dc4c3904e0fcd43a8bb6a276
015-Aman <asc23062000@gmail.com>
2023-11-20 00:22:40 +0530

    something i have changed (local mongo db compass)

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
-const mongoConnectionString = "mongodb+srv://Aman_Singh:Aman123@cluster0.yvl4tlu.mongodb.net/?retryWrites=true&w=majority"
+const mongoConnectionString = "mongodb://127.0.0.1:27017/malldb"
+// mongodb+srv://Aman_Singh:Aman123@cluster0.yvl4tlu.mongodb.net/?retryWrites=true&w=majority
 const secret = "12345"
 const adminRoutes = require("./routes/admin");
 const Product = require("./models/product");
